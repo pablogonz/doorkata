722fcf8323b3f3d8e70e9d6fea0bcebcec7fb45b
pablogonz <65257437+pablogonz@users.noreply.github.com>
2021-12-05 23:14:51 -0400

    push final
    kata finalizado

--- doorkata.js
+++ doorkata.js
@@ -10,5 +10,74 @@ this.IsLocked= IsLocked;
 this.IsUnlocked= IsUnlocked;
 this.key= key;
     }
+    get Open(){
+        return this.IsOpen;
+    }
+    get Close(){
+        return this.IsClosed;
 }
+get Lock(){
+    return this.IsLocked;
+}
+OpenAndUnlocked(key){
+    if (key !=this.key)return "key isn't match";
+    if(this.Open && this.IsUnlocked)
+    return "Error";
+    this.IsOpen=true;
+    this.IsClosed=false;
+    this.IsUnlocked=true;
+    this.IsLocked=false;
+    
+}
+
+OpenAndlocked(key){
+    if (key !=this.key)return "key isn't match";
+    if(this.Open && this.IsLocked)
+    return "Error";
+    this.IsOpen=true;
+    this.IsClosed=false;
+    this.IsUnlocked=false;
+    this.IsLocked=true;
+}
+ClosedAndUnlocked(key){
+    if (key !=this.key)return "key isn't match";
+    if(this.IsClosed && this.IsUnlocked)
+    return "Error";
+    this.IsOpen=false;
+    this.IsClosed=true;
+    this.IsUnlocked=true;
+    this.IsLocked=false;
+}
+ClosedAndlocked(key){
+    if (key !=this.key)return "key isn't match";
+    if(this.IsClosed && this.IsLocked)
+    return "Error";
+    this.IsOpen=false;
+    this.IsClosed=true;
+    this.IsUnlocked=false;
+    this.IsLocked=true;
+}
+ToString(){
+    return `${this.IsOpen},${this.IsClosed},${this.IsUnlocked},${this.IsLocked},${this.key}`
+}
+static OpenAndlocked(key){}
+static OpenAndUnlocked(key){}
+static ClosedAndUnlocked(key){}
+static ClosedAndlocked(key){}
+}
+//Forma imperativa de las puertas
+let door= new Door(true,false,false,true, "123")
+console.log(door.ToString());
+console.log(door.ClosedAndUnlocked("123"));
+console.log(door.ToString());
+
+let dooor= new Door(true,false,false,true, "123")
+console.log(dooor.ToString());
+console.log(dooor.OpenAndlocked("123"));
+console.log(dooor.ToString());
+
+let d= new Door(true,false,false,true, "123")
+console.log(d.ToString());
+console.log(d.ClosedAndlocked("123"));
+console.log(d.ToString());
 module.exports=Door;
--- doorkata.test.js
+++ doorkata.test.js
@@ -5,3 +5,18 @@ test('OpenAndUnlocked', () => {
 
   });
 
+const d2 = new Door(true,false,true,false,"123");
+test('OpenAndLocked', () => {
+    expect(d2).toEqual({"IsOpen":true, "IsClosed":false, "IsLocked":true, "IsUnlocked":false, "key":"123"});
+
+  });
+  const d3 = new Door(false,true,true,false,"123");
+  test('ClosedAndlocked', () => {
+      expect(d3).toEqual({"IsOpen":false, "IsClosed":true, "IsLocked":true, "IsUnlocked":false, "key":"123"});
+  
+    });
+    const d4 = new Door(false,true,false,true,"123");
+    test('ClosedAndUnlocked', () => {
+        expect(d4).toEqual({"IsOpen":false, "IsClosed":true, "IsLocked":false, "IsUnlocked":true, "key":"123"});
+    
+      });
