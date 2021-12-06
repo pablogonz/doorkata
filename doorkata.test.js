812fcc3212ad583f485b2d3e4c43fae6d753c02a
PABLO GONZALEZ TAVAREZ <1094765@est.intec.edu.do>
2021-12-05 20:32:59 -0400

    OpenAndLocked Push

--- doorkata.test.js
+++ doorkata.test.js
@@ -4,3 +4,10 @@ test('OpenAndUnlocked', () => {
     expect(d).toEqual({"IsOpen":true, "IsClosed":false, "IsLocked":false, "IsUnlocked":true, "key":"123"});
 
   });
+
+const d2 = new Door(true,false,true,false,"123");
+test('OpenAndLocked', () => {
+    expect(d2).toEqual({"IsOpen":true, "IsClosed":false, "IsLocked":true, "IsUnlocked":false, "key":"123"});
+
+  });
+  
